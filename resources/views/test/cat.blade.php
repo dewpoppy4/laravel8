17d5c50637903d533c110bf1e816ace843645208
dewpoppy4 <96884356+dewpoppy4@users.noreply.github.com>
2023-06-27 17:23:10 +0700

    Week 02

---
+++ resources/views/test/cat.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    img {
+        width: 250px;
+    }
+</style>
+
+<body>
+    <h1>รัชพล ยอดแก้ว 64122420006</h1>
+    ant<img src="{{$cat}}" alt="cat">
+</body>
+
+</html>
---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title></title>
+
+<style>
+    img {
+        width: 235px; 
+    }
+</style>
+</head>
+<body>
+    <h1>ratchapol yodkaew 64122420006</h1>
+    ant<a href="gallery/ant"><img src="{{$ant}}" alt="ant"> </a>
+    bird<a href="gallery/bird"><img src="{{$bird}}" alt="bird"> </a>
+    cat<a href="gallery/cat"><img src="{{$cat}}" alt="cat"> </a>
+    god<a href="gallery/god"><img src="{{$god}}" alt="god"> </a>
+    spider<a href="#"><img src="{{$spider}}" alt="spider"> </a>
+</body>
+
+</html>
